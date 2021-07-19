26dd5a1fb55730f6859708b58f44cf799b0a9796
aditya <amittal@equalexperts.com>
2021-07-19 17:15:15 +0530

    Add terraform for ECR provisioning

---
+++ ecr/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "0.12.19"
+
+  backend "s3" {
+    bucket  = "ee-pune-bootcamp-2021-tf-state"
+    key     = "ecr/us-east-1/terraform.tfstate"
+    encrypt = true
+
+    dynamodb_table = "ee-pune-bootcamp-2021-terraform-lock-table"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+locals {
+  name_prefix = "bootcamp-2021-ee-pune"
+  common_tags = {
+    CreatedBy             = "terraform"
+    MaintainerSlackHandle = "${local.name_prefix}"
+  }
+}
+
+resource "aws_ecr_repository" "jenkins" {
+  name = "${local.name_prefix}-ecr/jenkins"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = merge(
+    map(
+      "Name", "${local.name_prefix}-ecr/jenkins"
+    ),
+    local.common_tags
+  )
+}
