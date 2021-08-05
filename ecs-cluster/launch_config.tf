38f9e0432f4f924f1b03229529c45213a8cd765a
aditya <amittal@equalexperts.com>
2021-08-05 15:08:00 +0530

    Updated security group for ECS cluster

--- ecs-cluster/launch_config.tf
+++ ecs-cluster/launch_config.tf
@@ -33,6 +33,7 @@ resource "aws_launch_configuration" "ecs_launch_config" {
   instance_type        = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.ecs.name
   user_data            = data.template_file.ecs_userdata.rendered
+  security_groups      = [aws_security_group.cluster_sg.id]
 
   // try to define spot price
   #spot_price = "TBD"
