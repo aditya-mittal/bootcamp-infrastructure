5fc54de99d43d9fc742684afc0cf7c27732c3719
aditya <amittal@equalexperts.com>
2021-08-12 13:12:57 +0530

    Add root record set for domain name

--- alb/route53_record.tf
+++ alb/route53_record.tf
@@ -3,6 +3,18 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
+resource "aws_route53_record" "root" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = data.aws_route53_zone.main.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "service" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "api.${data.aws_route53_zone.main.name}"
