7eeaa469985a33c40db0f29327e69c19e3bdf56a
aditya <amittal@equalexperts.com>
2021-08-14 13:17:38 +0530

    Add route53 record for subdomain user.bootcamp2021.online

--- alb/route53_record.tf
+++ alb/route53_record.tf
@@ -3,6 +3,18 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
+resource "aws_route53_record" "root" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "REPLACE-USERNAME.${data.aws_route53_zone.main.name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "service" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "REPLACE-USERNAME.api.${data.aws_route53_zone.main.name}"
